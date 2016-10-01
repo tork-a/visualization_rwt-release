84cbdb82a1364ad70f18c5328a65c1b1c3dabb52
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2016-10-01 15:53:57 +0900

    Trimmed the branch to only the rwt_image_view sub directory

--- rwt_robot_monitor/www/rwt_robot_monitor_main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var ros = new ROSLIB.Ros({
-    url: "ws://" + location.hostname + ":8888"
-  });
-  var robot_monitor = new ROSLIB.RWTRobotMonitor({
-    ros: ros,
-    last_time_id: '#last-message-sec'
-  });
-});
