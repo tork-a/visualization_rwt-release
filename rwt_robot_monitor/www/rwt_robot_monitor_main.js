1fe4cb83715e3885ad77a2e25cd7d412a4a6ed9c
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-11-12 19:32:59 +0900

    Trimmed the branch to only the rwt_utils_3rdparty sub directory

--- rwt_robot_monitor/www/rwt_robot_monitor_main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var ros = new ROSLIB.Ros()
-  ros.install_config_button("config-button");
-
-  var robot_monitor = new ROSLIB.RWTRobotMonitor({
-    ros: ros,
-    last_time_id: '#last-message-sec'
-  });
-});
