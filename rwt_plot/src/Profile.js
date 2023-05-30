496f2eaf6ee0d3be5f78a970581b116e3596b221
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2023-05-30 15:58:50 +0900

    Trimmed the branch to only the rwt_robot_monitor sub directory

--- rwt_plot/src/Profile.js
+++
@@ -1,7 +0,0 @@
-ROSLIB.profile = function(func) {
-  var before = ROSLIB.Time.now();
-  func.call();
-  var after = ROSLIB.Time.now();
-  var diff = after.substract(before);
-  console.log('profile: ' + diff.toSec());
-};
